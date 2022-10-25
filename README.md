b2bf4bb54e3bdeee848eb30f611580618171a503
BrunoRagner <brunooragnner@gmail.com>
2022-10-25 10:35:44 -0300

    atualizaçao do  uplaod

--- README.md
+++
@@ -1,2 +0,0 @@
-# sistema-de-curriculo
-sistema desenvolvido em Flask para envio de curriculo 
--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, make_response
 from werkzeug.utils import secure_filename
 
 
 
-app = Flask(__name__)
 
+UPLOAD_FOLDER = '/home/bruno/Documentos/bruno/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
  
+app.logger.debug('Um valor para depuração')
+app.logger.warning('Ocorreu um avis (%d apples)', 42)
+app.logger.error('Ocorreu um erro')
+
 
 
 @app.route("/")
-def hello_world():
+def index():
    return render_template('index.html')
 
 
+@app.route("/")
+def Cookie():
+    resp = make_response(render_template(...))
+    resp.set_cookie('username', 'the username')
+    return resp
+
+
+
+@app.route("/vaga")
+def vaga():
+   return render_template("vaga.html")
+
+   
 @app.errorhandler(404) 
 def not_found(e): 
    return render_template("404.html") 
